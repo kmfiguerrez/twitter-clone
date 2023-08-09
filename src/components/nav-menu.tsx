edb5a4b85b46570ee5253b07a691913ea80fcf7e
kmfiguerrez <kmfiguerrez@gmail.com>
2023-08-09 15:03:28 +0800

    Add Shadcn components.
    - add button
    - dropdown menu

---
+++ src/components/nav-menu.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+  } from "@/components/ui/navigation-menu"
+import { ModeToggle } from "./theme-button"
+  
+
+export default function NavMenu() {
+    return (
+        <NavigationMenu>
+            <NavigationMenuList>
+                
+                <NavigationMenuItem>
+                <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                    <NavigationMenuLink>Link</NavigationMenuLink>
+                </NavigationMenuContent>
+                </NavigationMenuItem>
+                
+                <NavigationMenuItem>
+                    <ModeToggle></ModeToggle>
+                </NavigationMenuItem>
+            
+            </NavigationMenuList>            
+        </NavigationMenu>
+
+    )
+}
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,25 @@
+
+
+const items = [
+    {
+        label: 'Home',
+        href: '/',
+        icon: ''
+    },
+    {
+        label: 'Notifications',
+        href: '/notifications',
+        icon: ''
+    },
+    {
+        label: 'Profile',
+        href: '/users/123',
+        icon: ''
+    }
+]
+
+export default function Sidebar () {
+    return (
+        <p>Sidebar</p>
+    )
+}
